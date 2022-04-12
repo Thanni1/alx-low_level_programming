3a7753fff1d1cbbd67ccc25d0aef4a4b47c599b8
Thanni1 <doyin308@gmail.com>
2022-04-12 04:15:07 -0700

    my 6th task

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+void print_alphabet(void)
+{
+        int l = 'a';
+        while (l <= 'z')
+        {
+                _putchar(l);
+                l += 1;
+        }
+        _putchar(10);
+        return;
+	int l = 'a';
+
+	while (l <= 'z')
+	{
+		_putchar(l);
+		l += 1;
+	}
+	_putchar(10);
+}
