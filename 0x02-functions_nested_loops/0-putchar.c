00fe9c51b46d336c7142146c1c31fa397021edf7
Thanni1 <doyin308@gmail.com>
2022-04-12 03:52:45 -0700

    my 1st task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,14 @@
 #include "main.h"
 int main(void)
 {
-	char s[9] = "_putchar";
-	int i = 0;
+char s[9] = "_putchar";
+int i = 0;
 
-	while (i < 8)
-		{
-		_putchar (s[i]);
-		i++;
-		}
-		_putchar ('\n');
-	return (0);
+while (i < 8)
+{
+_putchar (s[i]);
+i++;
+}
+_putchar ('\n');
+return (0);
 }
